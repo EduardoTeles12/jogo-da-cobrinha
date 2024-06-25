ce01510fd5f0ad2a472323793241ee1cb984bb27
eduardo teles <jteles0985@gmail.com>
2024-06-25 14:32:50 -0300

    configuracoes da cobra e dos sons

--- snakegame.py
+++ snakegame.py
@@ -15,3 +15,18 @@ WINDOW_SIZE = (600, 400)
 GRID_SIZE = 20
 GRID_WIDTH = WINDOW_SIZE[0] // GRID_SIZE
 GRID_HEIGHT = WINDOW_SIZE[1] // GRID_SIZE
+
+# Configurações da cobra
+INITIAL_LENGTH = 3
+SNAKE_SPEED = 10
+
+# Direções
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+# Carregando os sons
+eat_sound = pygame.mixer.Sound("comendomaca.wav")
+game_over_sound = pygame.mixer.Sound("burro_-burro.wav")
+pygame.mixer.music.load("V E N O M - T Á F I C A N D O A P E R T A D O.mp3")
